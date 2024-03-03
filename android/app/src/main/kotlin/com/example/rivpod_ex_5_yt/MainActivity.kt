ff3112dafade5fd045206fd9172f4e1f6ed99e70
sachin supunthaka <sachinsupunthaka96@gmail.com>
2024-03-03 09:46:46 +0530

    init: flutter proj with riverpod

---
+++ android/app/src/main/kotlin/com/example/rivpod_ex_5_yt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rivpod_ex_5_yt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:rivpod_ex_5_yt/screens/home_screen.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
